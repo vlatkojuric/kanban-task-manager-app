414f7f869cf949b915c09ea4f0e37fc7324f31d0
vlatkojuric <juricv78@gmail.com>
2024-02-22 20:22:00 +0100

    added the data from the json file onto the UI of the web app,trying also to use the semantic tags as much as possible inside of this project

--- src/components/PlatformLaunch.js
+++ src/components/PlatformLaunch.js
@@ -1,12 +1,38 @@
 import data from "../data.json";
 
 export default function PlatfromLaunch() {
+  const platformLaunchTodo = data.boards[0].columns[0];
+  const platformLaunchDoing = data.boards[0].columns[1];
+  const platformLaunchDone = data.boards[0].columns[2];
+
   return (
-    <div>
-      <h1>Platform Launch</h1>
-      {data?.boards?.map((info) => (
-        <p>{info.name}</p>
-      ))}
-    </div>
+    <section>
+      <div>
+        <h1>{platformLaunchTodo.name}</h1>
+        <ul>
+          {platformLaunchTodo?.tasks?.map((task, index) => (
+            <li key={index}>{task.title}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <h1>{platformLaunchDoing.name}</h1>
+        <ul>
+          {platformLaunchDoing?.tasks?.map((task, index) => (
+            <li key={index}>{task.title}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <h1>{platformLaunchDone.name}</h1>
+        <ul>
+          {platformLaunchDone?.tasks.map((task, index) => (
+            <li key={index}>{task.title}</li>
+          ))}
+        </ul>
+      </div>
+    </section>
   );
 }
