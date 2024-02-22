b0ac8562c69a97d61d1535f555db39e6ba94421b
vlatkojuric <juricv78@gmail.com>
2024-02-22 22:02:22 +0100

    added all the data from the json file to the adequate components

--- src/components/MarketingPlan.js
+++ src/components/MarketingPlan.js
@@ -1,7 +1,38 @@
+import data from "../data.json";
+
 export default function MarketingPlan() {
+  const marketingPlanTodo = data?.boards[1].columns[0];
+  const marketingPlanDoing = data?.boards[1].columns[1];
+  const marketingPlanDone = data?.boards[1].columns[2];
+
   return (
-    <div>
-      <h1>Marketing Plan</h1>
-    </div>
+    <section>
+      <div>
+        <h1>{marketingPlanTodo.name}</h1>
+        <ul>
+          {marketingPlanTodo?.tasks?.map((task, index) => (
+            <li key={index}>{task.title}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <h1>{marketingPlanDoing.name}</h1>
+        {/* <ul>
+          {marketingPlanDoing?.tasks.map((task, index) => (
+            <li key={index}>{task.title}</li>
+          ))}
+        </ul> */}
+      </div>
+
+      <div>
+        <h1>{marketingPlanDone.name}</h1>
+        {/* <ul>
+          {marketingPlanDone?.tasks.map((task, index) => (
+            <li key={index}>{task.title}</li>
+          ))}
+        </ul> */}
+      </div>
+    </section>
   );
 }
--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -1,7 +1,26 @@
+import data from "../data.json";
+
 export default function Roadmap() {
+  const roadmapNow = data?.boards[2].columns[0];
+  const roadmapNext = data?.boards[2].columns[1];
+  const roadmapLater = data?.boards[2].columns[2];
   return (
-    <div>
-      <h1>Roadmap</h1>
-    </div>
+    <section>
+      <div>
+        <h1>{roadmapNow.name}</h1>
+        <ul>
+          {roadmapNow?.tasks?.map((task, index) => (
+            <li key={index}>{task.title}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <h1>{roadmapNext.name}</h1>
+      </div>
+      <div>
+        <h1>{roadmapLater.name}</h1>
+      </div>
+    </section>
   );
 }
