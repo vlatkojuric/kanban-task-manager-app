96dd0af1a6648fa3aab9b1f03ba6155a2e0c19ba
vlatkojuric <juricv78@gmail.com>
2024-02-24 21:40:44 +0100

    removed the borders,imported a pretty Google font

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import "../styles/Navbar.css";
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <p>All Boards(num)</p>
+      <p>All Boards(X)</p>
       <Link to="/">Platform Launch </Link>
       <Link to="/marketing-plan">Marketing Plan </Link>
       <Link to="/roadmap">Roadmap </Link>
