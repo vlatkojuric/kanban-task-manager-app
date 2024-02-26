1ba8476f0922855eceb58a46dbb1e228e445400d
vlatkojuric <juricv78@gmail.com>
2024-02-26 23:29:50 +0100

    centered the modal form in the middle of the web app when the button is clicked,also made the background dim a little bit with the semi-transparent black

--- src/components/AddNewBoardModal.js
+++ src/components/AddNewBoardModal.js
@@ -3,16 +3,16 @@ import "../styles/AddNewBoardModal.css";
 export default function AddNewBoardModal({ show }) {
   return (
     show && (
-      <section class="new_board_modal_form">
-        <form>
+      <section className="backgroundModal">
+        <form class="new_board_modal_form">
           <label for="name">Name:</label>
           <input type="text" id="name" name="name"></input>
           <label for="columns">Columns</label>
           <input type="text" id="columns" name="column1"></input>
           <input type="text" id="columns" name="column2"></input>
+          <button>+Add New Column</button>
+          <button>Create New Board</button>
         </form>
-        <button>+Add New Column</button>
-        <button>Create New Board</button>
       </section>
     )
   );
