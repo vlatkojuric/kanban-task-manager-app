d5caf7bd8e40d4f6628e58b549507816f48f2518
vlatkojuric <juricv78@gmail.com>
2024-03-01 14:43:04 +0100

    small changes to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,16 +5,21 @@ import AddIcon from "@mui/icons-material/Add";
 export default function Navbar({ handleShowFormClick, boardCount, navLink }) {
   return (
     <nav className="navbar">
-      <p>All Boards({boardCount})</p>
+      <div className="navbar_elements">
+        <p>All Boards({boardCount})</p>
 
-      {navLink.map((link) => (
-        <Link key={link.to} to={link.to}>
-          {link.name}
-        </Link>
-      ))}
-      <button onClick={handleShowFormClick}>
-        <AddIcon /> Create new board
-      </button>
+        {navLink.map((link) => (
+          <Link key={link.to} to={link.to}>
+            {link.name}
+          </Link>
+        ))}
+        <button onClick={handleShowFormClick}>
+          <AddIcon /> Create new board
+        </button>
+      </div>
+      <div>
+        <span className="hide_bar">hide bar</span>
+      </div>
     </nav>
   );
 }
