476654e1c068f3db7521bfd408f1be4077f1c008
vlatkojuric <juricv78@gmail.com>
2024-03-04 17:31:38 +0100

    styled the add new task button inside of the header,centered and made some space between elements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,34 @@
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 import AddNewTask from "../assets/AddNewTask";
+import LogoLight from "../assets/LogoLight";
 
 export default function Navbar({ handleShowFormClick, boardCount, navLink }) {
   return (
-    <nav className="navbar">
-      <div className="navbar_elements">
-        <p>All Boards({boardCount})</p>
+    <>
+      <div className="logo_navbar">
+        <LogoLight />
+        <p>kanban</p>
+      </div>
+      <nav className="navbar">
+        <div className="navbar_elements">
+          <p>All Boards({boardCount})</p>
 
-        {navLink.map((link) => (
-          <Link key={link.to} to={link.to} className="navbar_links">
-            {link.name}
-          </Link>
-        ))}
-        <div className="button_nav_container">
-          <button className="navbar_button" onClick={handleShowFormClick}>
-            <AddNewTask /> Create new board
-          </button>
+          {navLink.map((link) => (
+            <Link key={link.to} to={link.to} className="navbar_links">
+              {link.name}
+            </Link>
+          ))}
+          <div className="button_nav_container">
+            <button className="navbar_button" onClick={handleShowFormClick}>
+              <AddNewTask /> Create new board
+            </button>
+          </div>
         </div>
-      </div>
-      <div>
-        <span className="hide_bar">hide bar</span>
-      </div>
-    </nav>
+        <div>
+          <span className="hide_bar">hide bar</span>
+        </div>
+      </nav>
+    </>
   );
 }
