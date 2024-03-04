f4e3cac52cc8e5583bc155ad0a83cafdf5ccdf34
vlatkojuric <juricv78@gmail.com>
2024-03-04 18:24:55 +0100

    added the logo inside the navbar component and gave it some style,margin-top,centered the elements,size,gap between logo and the app name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,30 +5,28 @@ import LogoLight from "../assets/LogoLight";
 
 export default function Navbar({ handleShowFormClick, boardCount, navLink }) {
   return (
-    <>
-      <div className="logo_navbar">
-        <LogoLight />
-        <p>kanban</p>
-      </div>
-      <nav className="navbar">
-        <div className="navbar_elements">
-          <p>All Boards({boardCount})</p>
+    <nav className="navbar">
+      <div className="navbar_elements">
+        <span className="logo_navbar">
+          <LogoLight className="logo_light" />
+          <h3>ZenDo</h3>
+        </span>
+        <p>All Boards({boardCount})</p>
 
-          {navLink.map((link) => (
-            <Link key={link.to} to={link.to} className="navbar_links">
-              {link.name}
-            </Link>
-          ))}
-          <div className="button_nav_container">
-            <button className="navbar_button" onClick={handleShowFormClick}>
-              <AddNewTask /> Create new board
-            </button>
-          </div>
+        {navLink.map((link) => (
+          <Link key={link.to} to={link.to} className="navbar_links">
+            {link.name}
+          </Link>
+        ))}
+        <div className="button_nav_container">
+          <button className="navbar_button" onClick={handleShowFormClick}>
+            <AddNewTask /> Create new board
+          </button>
         </div>
-        <div>
-          <span className="hide_bar">hide bar</span>
-        </div>
-      </nav>
-    </>
+      </div>
+      <div>
+        <span className="hide_bar">hide bar</span>
+      </div>
+    </nav>
   );
 }
